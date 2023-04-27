32acafa5e08784775db88ec1c498178a461f5c45
Martijn Pieters <mj@zopatista.com>
2023-04-27 16:11:59 +0100

    Correct type hint: namespace is not optional
    The namespace argument is positional and will never be `None` so should
    not be marked as Optional. It is always a string, and the default is
    to pass in an empty string.

--- fastapi_cache/key_builder.py
+++ fastapi_cache/key_builder.py
@@ -7,7 +7,7 @@ from starlette.responses import Response
 
 def default_key_builder(
     func: Callable,
-    namespace: Optional[str] = "",
+    namespace: str = "",
     request: Optional[Request] = None,
     response: Optional[Response] = None,
     args: Optional[tuple] = None,
